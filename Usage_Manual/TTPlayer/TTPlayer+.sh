577c7fe8e7a437febda36c1c04f6bf534fcdcdaa
7900ms <7900ms@users.noreply.github.com>
2017-05-05 14:28:16 +0800

    Create TTPlayer+.sh

---
+++ Usage_Manual/TTPlayer/TTPlayer+.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# chmod +x script.sh
+
+cp /Users/sysadmin/Library/Application\ Support/CrossOver/Bottles/win7/drive_c/Program\ Files/TTPlayer/AddIn/ttp_lrcsh\ .ini副本 /Users/sysadmin/Library/Application\ Support/CrossOver/Bottles/win7/drive_c/Program\ Files/TTPlayer/AddIn/ttp_lrcsh\ .ini
+echo 'done'
+exit
